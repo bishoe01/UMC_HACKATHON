d608a264918cdd7e5ec04ba742d3b34b454f6744
bishoe01 <bishoe01@gmail.com>
2023-07-03 13:28:59 +0900

    개발 초기환경 세팅

---
+++ starter/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1>Error</h1>,
+    children: [
+      { index: true, path: '/', element: <Home /> },
+      { path: '/about', element: <h1>About</h1> },
+    ]
+  }
+]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <RouterProvider router={router} />
+);
+
+reportWebVitals();
