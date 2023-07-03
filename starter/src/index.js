62c0394b239f8507b5da2390d078c8b414f3e4c9
gs0428 <lider28@naver.com>
2023-07-03 21:16:36 +0900

    세팅

--- starter/src/index.js
+++ starter/src/index.js
@@ -1,27 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
-import Profile from './pages/Profile';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <h1>Error</h1>,
     children: [
-      { index: true, path: '/', element: <Home /> },
-      { path: '/about', element: <h1>About</h1> },
-      { path: '/profile', element: <Profile /> },
-    ]
-  }
+      { index: true, path: "/", element: <Home /> },
+      { path: "/about", element: <h1>About</h1> },
+      { path: "/profile", element: <Profile /> },
+    ],
+  },
 ]);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <RouterProvider router={router} />
-);
-
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
